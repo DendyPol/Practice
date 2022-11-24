d8619634ca0525ff77caa47b357a4d65daaaf7ff
DendyPol <Polovinko_91@icloud.com>
2022-11-24 16:46:04 +0300

    task one, two, add three

--- HomeWorkTwo.java
+++ HomeWorkTwo.java
@@ -5,6 +5,7 @@ public class HomeWorkTwo {
     public static void main(String[] args) {
         taskOne();
         taskTwo();
+        taskThree();
     }
 
     private static void taskOne() {
@@ -27,6 +28,18 @@ public class HomeWorkTwo {
             b[i] = k;
 
             System.out.println(b[i]);
+        }
+    }
+
+    private static void taskThree() {
+        int[] a = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        int b = a.length;
+
+        for (int i = 0; i < b; i++) {
+            if (a[i] < 6) {
+                a[i] = a[i] * 2;
+                System.out.println(a[i]);
+            }
         }
     }
 }
