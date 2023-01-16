1d8393b515245cc11a7df22bf1812f22c59a287c
DendyPol <Polovinko_91@icloud.com>
2023-01-16 18:38:40 +0300

    HomeWorkFive remove multi add construct

--- HomeWorkFive/Person.java
+++ HomeWorkFive/Person.java
@@ -1,12 +1,13 @@
 package HomeWorkFive;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@AllArgsConstructor
 public class Person {
-  private Object message;
   private String surname;
   private String name;
   private String patronymic;
@@ -16,29 +17,15 @@ public class Person {
   private double salary;
   private int age;
 
-  public Person(String surname, String name, String patronymic,
-                String position, String email, String phone, double salary, int age) {
-    this.surname = surname;
-    this.name = name;
-    this.patronymic = patronymic;
-    this.position = position;
-    this.email = email;
-    this.phone = phone;
-    this.salary = salary;
-    this.age = age;
-  }
-
   public void printInfo() {
-    String message =
-      """
-        Фамилия:""" + this.surname + """
-        " Имя:""" + this.name + """
-        " Отчество:""" + this.patronymic + """
-        " Должность:""" + this.position + """
-        " Почта:""" + this.email + """
-        " Телефон:""" + this.phone + """
-        " Зар.Плата:""" + this.salary + """
-        " Возраст:""" + this.age;
+    String message = String.format("%s\n%s\n%s\n%s\n%s\n%s\n%s"
+      , "Фамилия: " + this.surname
+      , "Имя: " + this.name
+      , "Отчество: " + this.patronymic
+      , "Должность: " + this.position
+      , "Почта: " + this.email + this.phone
+      , "Зар.Плата: " + this.salary
+      , "Возраст: " + this.age);
     System.out.println(message);
   }
 }
